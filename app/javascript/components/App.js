830f8f392a859c40a78f9aabdc87e824e422922f
levy002 <levy.ukwi002@gmail.com>
2022-08-09 14:23:24 +0200

    Creating app component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Greeting from './Greeting';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
